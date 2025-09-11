aa7a17aac04ac9f8c84c83494f8029fdc1be9517
owx0130 <weixiang0130@gmail.com>
2025-09-11 10:31:22 +0800

    working implementation w/ ultra96

--- AI/Hardware/mlp.cpp
+++ AI/Hardware/mlp.cpp
@@ -1,30 +1,34 @@
-#include <iostream>
-#include <cmath>
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 #include "mlp_weights.h"
 
-typedef ap_axiu<32,0,0,0> axis_word;
+typedef union {
+    float f;
+    unsigned int i;
+} fp_conv;
 
-// ReLU activation
+typedef ap_axiu<32, 0, 0, 0> axis_word;
+
 float relu(float x) {
     return x > 0 ? x : 0;
 }
 
-// MLP forward pass
 void mlp_forward(hls::stream<axis_word> &in_stream, hls::stream<axis_word> &out_stream) {
-    #pragma HLS INTERFACE axis port=in_stream
-    #pragma HLS INTERFACE axis port=out_stream
-    #pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS INTERFACE axis port=in_stream
+#pragma HLS INTERFACE axis port=out_stream
+#pragma HLS INTERFACE ap_ctrl_none port=return
 
-    float input[INPUT_SIZE];
-    float hidden[HIDDEN_SIZE];
-    float output[OUTPUT_SIZE];
+    float input[INPUT_SIZE] = {0};
+    float hidden[HIDDEN_SIZE] = {0};
+    float output[OUTPUT_SIZE] = {0};
 
+    axis_word pkt;
+    fp_conv converter;
     // Read INPUT_SIZE values from AXI stream
-    for (int j = 0; j < INPUT_SIZE; j++) {
-        axis_word pkt = in_stream.read();
-        input[j] = *(float*)&pkt.data; // read float from TDATA
+    for (int i = 0; i < INPUT_SIZE; i++) {
+        in_stream.read(pkt);
+        converter.i = pkt.data;
+        input[i] = converter.f;
     }
 
     // Layer 1
@@ -46,9 +50,9 @@ void mlp_forward(hls::stream<axis_word> &in_stream, hls::stream<axis_word> &out_
     
     // Write OUTPUT_SIZE values to AXI stream
     for (int i = 0; i < OUTPUT_SIZE; i++) {
-        axis_word pkt;
-        pkt.data = *(ap_uint<32>*)&output[i]; // convert float to 32-bit TDATA
-        pkt.last = (i == OUTPUT_SIZE-1) ? 1 : 0; // mark last word
+        converter.f = output[i];
+        pkt.data = converter.i;
+        pkt.last = (i == (OUTPUT_SIZE - 1)) ? 1 : 0;
         out_stream.write(pkt);
     }
 }
--- AI/Hardware/test.cpp
+++ AI/Hardware/test.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <cmath>
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 using namespace std;
@@ -8,7 +6,12 @@ using namespace std;
 #define HIDDEN_SIZE 3
 #define OUTPUT_SIZE 3
 
-typedef ap_axiu<32,0,0,0> axis_word;
+typedef union {
+    float f;
+    unsigned int i;
+} fp_conv;
+
+typedef ap_axiu<32, 0, 0, 0> axis_word;
 
 void mlp_forward(hls::stream<axis_word> &in_stream, hls::stream<axis_word> &out_stream);
 
@@ -16,25 +19,23 @@ int main() {
     hls::stream<axis_word> in_stream;
     hls::stream<axis_word> out_stream;
 
+    fp_conv converter;
+    axis_word pkt;
+
     float sample_input[INPUT_SIZE] = {1.0, -0.5, 0.3, 0.0, 0.0};
 
     for (int i = 0; i < INPUT_SIZE; i++) {
-        axis_word pkt;
-        pkt.data = *(ap_uint<32>*)&sample_input[i];   // reinterpret float bits as uint32
-        pkt.last = (i == INPUT_SIZE - 1) ? 1 : 0;     // mark last element
+        converter.f = sample_input[i];
+        pkt.data = converter.i;
         in_stream.write(pkt);
     }
 
-    // Call your HLS MLP function
     mlp_forward(in_stream, out_stream);
 
-    // Read and unpack output floats
-    cout << "MLP Output:" << endl;
     for (int i = 0; i < OUTPUT_SIZE; i++) {
-        axis_word pkt = out_stream.read();
-        float val = *(float*)&pkt.data;  // reinterpret back to float
-        cout << val << endl;
+        out_stream.read(pkt);
+        converter.i = pkt.data;
+        float out_val = converter.f;
+        cout << out_val << endl;
     }
-
-    return 0;
 }
