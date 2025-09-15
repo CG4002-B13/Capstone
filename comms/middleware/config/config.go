eb5fd4c78d868e6a8a56d018f36cf564eb52d49d
Davian Kho <daviankho1@gmail.com>
2025-09-16 02:12:46 +0800

    feat: add main logic and env to middleware

---
+++ comms/middleware/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	CACertLocation     string
+	ServerCertLocation string
+	ServerKeyLocation  string
+	WebSocketPort      string
+}
+
+func LoadConfig() *Config {
+	caCert := os.Getenv("CA_CERT")
+	serverCert := os.Getenv("SERVER_CERT")
+	serverKey := os.Getenv("SERVER_KEY")
+	WSPort := os.Getenv("WS_PORT")
+
+	if caCert == "" {
+		caCert = "../secrets/ca.crt"
+	}
+
+	if serverCert == "" {
+		serverCert = "../secrets/server.crt"
+	}
+
+	if serverKey == "" {
+		serverKey = "../secrets/server.key"
+	}
+
+	if WSPort == "" {
+		WSPort = "8443"
+	}
+
+	return &Config{
+		CACertLocation:     caCert,
+		ServerCertLocation: serverCert,
+		ServerKeyLocation:  serverKey,
+		WebSocketPort:      WSPort,
+	}
+}
