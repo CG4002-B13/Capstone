80bb5431b21e7df71031c8e74d0b51afd31cd27f
Davian Kho <daviankho1@gmail.com>
2025-09-24 01:05:44 +0800

    feat: add mqtt client to websocket

--- comms/middleware/config/config.go
+++ comms/middleware/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -11,7 +12,13 @@ type Config struct {
 	CACertLocation     string
 	ServerCertLocation string
 	ServerKeyLocation  string
+	ClientCertLocation string
+	ClientKeyLocation  string
 	WebSocketPort      string
+	MQTTHost           string
+	MQTTPort           int
+	MQTTUser           string
+	MQTTPass           string
 }
 
 func LoadConfig() *Config {
@@ -23,28 +30,61 @@ func LoadConfig() *Config {
 	caCert := os.Getenv("CA_CERT")
 	serverCert := os.Getenv("SERVER_CERT")
 	serverKey := os.Getenv("SERVER_KEY")
+	clientCert := os.Getenv("CLIENT_CERT")
+	clientKey := os.Getenv("CLIENT_KEY")
 	WSPort := os.Getenv("WS_PORT")
+	mqttHost := os.Getenv("MQTT_HOST")
+	mqttPortStr := os.Getenv("MQTT_PORT")
+	mqttUser := os.Getenv("MQTT_USER")
+	mqttPass := os.Getenv("MQTT_PASS")
 
 	if caCert == "" {
-		caCert = "../secrets/dev/visualiser-ca.crt"
+		caCert = "../secrets/dev/devices-ca.crt"
 	}
 
 	if serverCert == "" {
-		serverCert = "../secrets/dev/visualiser-server.crt"
+		serverCert = "../secrets/dev/devices-server.crt"
 	}
 
 	if serverKey == "" {
-		serverKey = "../secrets/dev/visualiser-server.key"
+		serverKey = "../secrets/dev/devices-server.key"
+	}
+
+	if clientCert == "" {
+		clientCert = "../secrets/devs/devices-server.crt"
+	}
+
+	if clientKey == "" {
+		clientKey = "../secrets/dev/devices-server.key"
 	}
 
 	if WSPort == "" {
 		WSPort = "8443"
 	}
 
+	if mqttHost == "" {
+		mqttHost = "127.0.0.1"
+	}
+
+	if mqttPortStr == "" {
+		mqttPortStr = "1883"
+	}
+
+	mqttPort, err := strconv.Atoi(mqttPortStr)
+	if err != nil {
+		log.Fatalf("Invalid MQTT_PORT: %v", err)
+	}
+
 	return &Config{
 		CACertLocation:     caCert,
 		ServerCertLocation: serverCert,
 		ServerKeyLocation:  serverKey,
+		ClientCertLocation: clientCert,
+		ClientKeyLocation:  clientKey,
 		WebSocketPort:      WSPort,
+		MQTTHost:           mqttHost,
+		MQTTPort:           mqttPort,
+		MQTTUser:           mqttUser,
+		MQTTPass:           mqttPass,
 	}
 }
--- comms/middleware/internal/mqtt_client.go
+++ comms/middleware/internal/mqtt_client.go
@@ -1,1 +1,94 @@
 package internal
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+type MQTTClient struct {
+	Client mqtt.Client
+	Host   string
+	Port   int
+	User   string
+	Pass   string
+}
+
+type MQTTTLSConfig struct {
+	CACertFile     string
+	ClientCertFile string
+	ClientKeyFile  string
+}
+
+func SetupMQTTTLS(config MQTTTLSConfig) (*tls.Config, error) {
+	caCertPool, err := LoadCACert(config.CACertFile)
+	if err != nil {
+		return nil, err
+	}
+
+	clientCert, err := LoadKeyPair(config.ClientCertFile, config.ClientKeyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{
+		RootCAs:      caCertPool,
+		Certificates: []tls.Certificate{clientCert},
+		MinVersion:   tls.VersionTLS12,
+	}, nil
+}
+
+func NewMQTTClient(clientID string, host string, port int, user string, pass string, tlsConfig *tls.Config) *MQTTClient {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("ssl://%s:%d", host, port))
+	opts.SetClientID(clientID)
+	if user != "" {
+		opts.SetUsername(user)
+		opts.SetPassword(pass)
+	}
+
+	opts.SetTLSConfig(tlsConfig)
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetPingTimeout(1 * time.Second)
+	opts.OnConnect = func(c mqtt.Client) {
+		log.Printf("Connected to MQTT broker at %s:%v", host, port)
+	}
+	opts.OnConnectionLost = func(c mqtt.Client, err error) {
+		log.Printf("Connection lost: %v\n", err)
+	}
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Failed to connect to MQTT Broker: %v", token.Error())
+	}
+
+	return &MQTTClient{
+		Client: client,
+		Host:   host,
+		Port:   port,
+		User:   user,
+		Pass:   pass,
+	}
+}
+
+// Subscribe to a topic with a callback
+func (m *MQTTClient) Subscribe(topic string, qos byte, callback mqtt.MessageHandler) {
+	token := m.Client.Subscribe(topic, qos, callback)
+	token.Wait()
+	if token.Error() != nil {
+		log.Fatalf("Subscribe error: %v", token.Error())
+	}
+	log.Printf("Subscribed to topic: %s", topic)
+}
+
+// Publish a message to a topic
+func (m *MQTTClient) Publish(topic string, qos byte, retained bool, payload interface{}) {
+	token := m.Client.Publish(topic, qos, retained, payload)
+	token.Wait()
+	if token.Error() != nil {
+		log.Printf("Publish error: %v", token.Error())
+	}
+}
