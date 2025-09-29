75a31daed433f69ca4e1a47f728ef6f6e0b7babf
Davian Kho <Daviankho1@gmail.com>
2025-09-30 02:50:41 +0800

    fix: add login step to allow ECR pulling

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,6 +5,9 @@ set -e
 
 cd /home/ubuntu/app/comms || exit 1
 
+echo "Logging into AWS ECR..."
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 828171332858.dkr.ecr.ap-southeast-1.amazonaws.com
+
 echo "Pulling latest images from ECR..."
 sudo docker compose -f docker-compose.yml pull
 
