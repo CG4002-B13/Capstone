71ddc6d258e26c181fc7cdd8d3acd3023c81d3ea
Davian Kho <Daviankho1@gmail.com>
2025-09-30 03:04:58 +0800

    fix: authentication issue with docker deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,11 +3,14 @@
 ### Script for AWS CodeDeploy to execute pre-install ###
 set -e
 
+AWS_REGION="ap-southeast-1"
+AWS_ACCOUNT_ID="828171332858"
+ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+
 cd /home/ubuntu/app/comms || exit 1
 
 echo "Logging into AWS ECR..."
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 828171332858.dkr.ecr.ap-southeast-1.amazonaws.com
-
+aws ecr get-login-password --region ${AWS_REGION} | sudo docker login --username AWS --password-stdin ${ECR_REGISTRY}
 echo "Pulling latest images from ECR..."
 sudo docker compose -f docker-compose.yml pull
 
