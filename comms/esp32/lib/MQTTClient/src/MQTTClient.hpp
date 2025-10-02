19a1e26c5bde3261f4e3ba953519e8c5a6d5c78f
Davian Kho <Daviankho1@gmail.com>
2025-10-02 13:52:46 +0800

    feat: add support for school wifi

--- comms/esp32/lib/MQTTClient/src/MQTTClient.hpp
+++ comms/esp32/lib/MQTTClient/src/MQTTClient.hpp
@@ -9,8 +9,8 @@ const char MQTT_HOST[] = MQTT_SERVER;
 const char MQTT_PORT_NUMBER[] = MQTT_PORT;
 const char MQTT_USERNAME[] = MQTT_USER;
 const char MQTT_PASSWORD[] = MQTT_PASS;
-const char MQTT_STATUS_TOPIC[] = "esp32/status";
-const char MQTT_COMMAND_TOPIC[] = "esp32/command";
+const char MQTT_STATUS_TOPIC[] = "/status";
+const char MQTT_COMMAND_TOPIC[] = "/command";
 
 const unsigned long MQTT_RECONNECT_DELAY = 5000;
 const unsigned long MESSAGE_PUBLISH_INTERVAL = 3000;
