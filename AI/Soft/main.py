e70d997e9a5b5acca0b88a9fb86172b83a753a85
owx0130 <weixiang0130@gmail.com>
2025-10-02 16:47:04 +0800

    clean up folder

---
+++ AI/Soft/main.py
@@ -0,0 +1,30 @@
+import time
+from config import MQTT_HOST, MQTT_PORT, MQTT_USER, MQTT_PASS
+from mqtt_client import SecureMQTTClient
+
+
+def main():
+    mqtt_client = SecureMQTTClient(
+        username=MQTT_USER,
+        password=MQTT_PASS,
+        host=MQTT_HOST,
+        port=MQTT_PORT,
+        clientId="ultra96",
+    )
+    mqtt_client.connect()
+    mqtt_client.subscribe(topic="/voice_data")
+    
+    
+    # Convert message to JSON
+    # data = ""
+    # mqtt_client.publish(topic="/voice_result", payload=data)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        mqtt_client.disconnect()
+
+
+if __name__ == "__main__":
+    main()
