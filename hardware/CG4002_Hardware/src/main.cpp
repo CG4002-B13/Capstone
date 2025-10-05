6a80676eebf4a36c2666fd64da471506535e2fb6
LowTL <e0957642@u.nus.edu>
2025-10-05 23:20:49 +0800

    Add button debounce

--- hardware/CG4002_Hardware/src/main.cpp
+++ hardware/CG4002_Hardware/src/main.cpp
@@ -91,78 +91,89 @@ void loop() {
     mpu_loop(mpu);
     unsigned long now = millis();
     static unsigned long button_debounce = 0;
-    if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == LOW) {
-        // screenshot
-    }
-    switch (button_state) {
-    case 0:
-        // No item selected
-        if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == HIGH) {
-            tone(BUZZER, NOTE_D5, NOTE_DURATION); // select an item
-            String message = "SELECT";
-            //mqttClient.publish(topic2, message.c_str());
-            // if acknowledge, send another buzz
-            Serial.println(message);
-            button_state = 1;
-        } else if (digitalRead(BUTTON_2) == LOW && digitalRead(BUTTON_1 == HIGH)) {
-            size_t total_samples = SAMPLING_RATE;
-            size_t samples_recorded = 0;
-            tone(BUZZER, NOTE_C5, NOTE_DURATION); // start recording
-            while (samples_recorded < total_samples) {
-                size_t bytes_read = 0;
-                esp_err_t ret = i2s_read(I2S_NUM_0, data_buffer, sizeof(data_buffer), &bytes_read, 100/portTICK_PERIOD_MS);
-                if (ret == ESP_OK && bytes_read > 0) {
-                    size_t samples = bytes_read / 2; //16-bit samples
-                    for (size_t i = 0; i < samples && samples_recorded < total_samples; i++, samples_recorded++) {
-                        int16_t sample16 = data[i];
-                        Serial.print(sample16);
+    if (now - button_debounce > DEBOUNCE) {
+        button_debounce = now;
+        if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == LOW) {
+            String message = "SCREENSHOT";
+            mqttClient.publish(topic2, message.c_str());
+        }
+        switch (button_state) {
+        case 0:
+            // No item selected
+            if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == HIGH) {
+                tone(BUZZER, NOTE_D5, NOTE_DURATION); // select an item
+                String message = "SELECT";
+                // mqttClient.publish(topic2, message.c_str());
+                //  if acknowledge, send another buzz
+                Serial.println(message);
+                button_state = 1;
+            } else if (digitalRead(BUTTON_2) == LOW &&
+                       digitalRead(BUTTON_1 == HIGH)) {
+                size_t total_samples = SAMPLING_RATE;
+                size_t samples_recorded = 0;
+                tone(BUZZER, NOTE_C5, NOTE_DURATION); // start recording
+                while (samples_recorded < total_samples) {
+                    size_t bytes_read = 0;
+                    esp_err_t ret =
+                        i2s_read(I2S_NUM_0, data_buffer, sizeof(data_buffer),
+                                 &bytes_read, 100 / portTICK_PERIOD_MS);
+                    if (ret == ESP_OK && bytes_read > 0) {
+                        size_t samples = bytes_read / 2; // 16-bit samples
+                        for (size_t i = 0;
+                             i < samples && samples_recorded < total_samples;
+                             i++, samples_recorded++) {
+                            int16_t sample16 = data[i];
+                            Serial.print(sample16);
+                        }
                     }
+                    yield();
                 }
-                yield();
+                delay(100); // to allow data to be stored properly
+                mqttClient.publish(topic0, "Incoming voice message");
+                char buffer[SAMPLING_RATE * sizeof(int16_t)];
+                memcpy(buffer, data, sizeof(buffer));
+                Serial.println(buffer);
+                mqttClient.publish(topic0, buffer);
+                mqttClient.publish(topic0, "Completed sending voice message");
+                tone(BUZZER, NOTE_C6, NOTE_DURATION); // completed recording
             }
-            delay(100); // to allow data to be stored properly
-            //mqttClient.publish(topic0, "Incoming voice message");
-            // char buffer[SAMPLING_RATE * sizeof(int16_t)];
-            // memcpy(buffer, data, sizeof(buffer));
-            // Serial.println(buffer);
-            //mqttClient.publish(topic0, buffer);
-            //mqttClient.publish(topic0, "Completed sending voice message");
-            tone(BUZZER, NOTE_C6, NOTE_DURATION); // completed recording
+            break;
+        case 1:
+            // Item selected
+            static unsigned long last_time = 0;
+            if (now - last_time >
+                DEBOUNCE) { // can deal w the debounce calibration later
+                last_time = now;
+                String message = "";
+                message += "Pitch=%6.2f  Roll=%6.2f  Yaw=%6.2f\n", pitch_deg,
+                    roll_deg, yaw_deg;
+                message += "AccX=%6.2f  AccY=%6.2f  AccZ=%6.2f", AccX, AccY,
+                    AccZ;
+                // figure out how to move objects using this
+                // mqttClient.publish(topic2, message.c_str());
+                Serial.print("Sent message: " + message);
+            }
+            if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == HIGH) {
+                // deselect item
+                tone(BUZZER, NOTE_D5, NOTE_DURATION); // select an item
+                String message = "DESELECT";
+                Serial.println(message);
+                // mqttClient.publish(topic2, message.c_str());
+                //  if acknowledge, send another buzz
+                button_state = 0;
+            } else if (digitalRead(BUTTON_2) == LOW &&
+                       digitalRead(BUTTON_1) == HIGH) {
+                String message = "TOGGLE"; // toggle between rotate and move
+            }
+            break;
+        default:
+            break;
         }
-        break;
-    case 1:
-        // Item selected
-        static unsigned long last_time = 0;
-        if (now - last_time > DEBOUNCE) { // can deal w the debounce calibration later
-            last_time = now;
-            String message = "";
-            message += "Pitch=%6.2f  Roll=%6.2f  Yaw=%6.2f\n", pitch_deg,
-                roll_deg, yaw_deg;
-            message += "AccX=%6.2f  AccY=%6.2f  AccZ=%6.2f", AccX, AccY, AccZ;
-            // figure out how to move objects using this
-            //mqttClient.publish(topic2, message.c_str());
-            Serial.print("Sent message: " + message);
-        }
-        if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == HIGH) {
-            // deselect item
-            tone(BUZZER, NOTE_D5, NOTE_DURATION); // select an item
-            String message = "DESELECT";
-            Serial.println(message);
-            //mqttClient.publish(topic2, message.c_str());
-            // if acknowledge, send another buzz
-            button_state = 0;
-        } else if (digitalRead(BUTTON_2) == LOW &&
-                   digitalRead(BUTTON_1) == HIGH) {
-            String message = "TOGGLE"; //toggle between rotate and move
-        }
-        break;
-    default:
-        break;
     }
     // Output (rate-limit to keep serial readable)
     static unsigned long lastMillis = 0;
     if ((now - lastMillis) > DEBOUNCE) { // only need to update battery
-                                              // percentage once in a while
+                                         // percentage once in a while
         lastMillis = now;
         String message = "voltage: ";
         message += battMonitor.readVoltage();
